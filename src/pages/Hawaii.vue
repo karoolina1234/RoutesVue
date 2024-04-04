81db7e7293fea813c95f4cc50df0c7ca6bbc7ad8
karoolina1234 <karoolina.sm@gmail.com>
2024-04-04 17:53:33 -0300

    reafatorando rotas

--- src/pages/Hawaii.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="hawaii">
-    <h1>Havawii</h1>
-  </div>
-</template>
--- src/router/index.js
+++ src/router/index.js
@@ -3,29 +3,9 @@ import Home from "@/pages/Home.vue";
 
 const routes = [
   { path: "/", name: "Home", component: Home },
-  {
-    path: "/brazil",
-    name: "Brazil",
-    component: () => import("@/pages/Brazil.vue"),
-  },
-  {
-    path: "/hawaii",
-    name: "Hawaii",
-    component: () => import("@/pages/Hawaii.vue"),
-  },
-  {
-    path: "/jamaica",
-    name: "Jamaica",
-    component: () => import("@/pages/Jamaica.vue"),
-  },
-  {
-    path: "/panama",
-    name: "Panama",
-    component: () => import("@/pages/Panama.vue"),
-  },
 
   {
-    path: "/vue-school-travel-app/:id",
+    path: "/vue-school-travel-app/:id/:slug",
     name: "destination.show",
     component: () => import("@/pages/DestinationShow.vue"),
   },
