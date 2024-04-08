44e8057573fd5f4007a5ebd0a07a1fb954a08a9c
karoolina1234 <karoolina.sm@gmail.com>
2024-04-08 13:25:55 -0300

    add efeito de scroll ao mudar de pagina

--- src/router/index.js
+++ src/router/index.js
@@ -36,6 +36,14 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+  scrollBehavior(to, form, savedPosition) {
+    return (
+      savedPosition ||
+      new Promise((resolve) => {
+        setTimeout(() => resolve({ top: 0, behavior: "smooth" }), 300);
+      })
+    );
+  },
 });
 
 export default router;
