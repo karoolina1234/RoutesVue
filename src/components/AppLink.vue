3c0b63a6815bbb0313ecbf8ab10842d17149749b
karoolina1234 <karoolina.sm@gmail.com>
2024-04-09 10:07:56 -0300

    composition API

--- src/components/AppLink.vue
+++ src/components/AppLink.vue
@@ -7,9 +7,10 @@
     :href="to"
     ><slot
   /></a>
-  <router-link v-else v-bind="$props" class="internal-link"><slot /></router-link>
+  <router-link v-else v-bind="$props" class="internal-link"
+    ><slot
+  /></router-link>
 </template>
-
 <script>
 import { RouterLink } from "vue-router";
 export default {
